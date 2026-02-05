39c619638a457939dc0420048652b522788904fc
Stefan Ernst <stefanernst@MBP-14-M4.local>
2026-02-05 17:12:59 +0100

    Removing unused files

--- frontend/src/lib/composables/index.js
+++ frontend/src/lib/composables/index.js
@@ -1,10 +1,2 @@
 export { createAsyncLoader } from './createAsyncLoader.svelte.js';
-export {
-  getBlurClass,
-  getItemBackground,
-  getItemBorder,
-  useCollectionStyles,
-} from './useCollectionStyles.svelte.js';
 export { useForm, validators } from './useForm.svelte.js';
-export { useItemEditing } from './useItemEditing.svelte.js';
-export { useItemLinks } from './useItemLinks.svelte.js';
