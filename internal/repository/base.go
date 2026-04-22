b7ca585aba2eeb43b547f120f818e9116468482e
Stefan Ernst <ernst.stefan@icloud.com>
2026-04-22 11:58:25 +0200

    repository: drop unused DynamicUpdateBuilder (duplicated type)
    The restapi/v1/handlers package has its own DynamicUpdateBuilder that is
    actually used; the repository-package copy had no callers and was pure
    duplication. Delete it.

--- internal/repository/base.go
+++ internal/repository/base.go
@@ -2,8 +2,6 @@ package repository
 
 import (
 	"database/sql"
-	"fmt"
-	"strings"
 
 	"windshift/internal/database"
 )
@@ -41,92 +39,6 @@ func ApplyActionNullFieldsToPtr(
 		val := int(createdVal.Int64)
 		*createdBy = &val
 	}
-}
-
-// DynamicUpdateBuilder helps build parameterized UPDATE statements dynamically.
-type DynamicUpdateBuilder struct {
-	sets []string
-	args []interface{}
-}
-
-// NewDynamicUpdateBuilder creates a new dynamic update builder.
-func NewDynamicUpdateBuilder() *DynamicUpdateBuilder {
-	return &DynamicUpdateBuilder{}
-}
-
-// AddBool adds a boolean field update if the value is non-nil.
-func (b *DynamicUpdateBuilder) AddBool(field string, value *bool) {
-	if value != nil {
-		b.sets = append(b.sets, field+" = ?")
-		b.args = append(b.args, *value)
-	}
-}
-
-// AddInt adds an integer field update if the value is non-nil.
-func (b *DynamicUpdateBuilder) AddInt(field string, value *int) {
-	if value != nil {
-		b.sets = append(b.sets, field+" = ?")
-		b.args = append(b.args, *value)
-	}
-}
-
-// AddInt64 adds an int64 field update if the value is non-nil.
-func (b *DynamicUpdateBuilder) AddInt64(field string, value *int64) {
-	if value != nil {
-		b.sets = append(b.sets, field+" = ?")
-		b.args = append(b.args, *value)
-	}
-}
-
-// AddString adds a string field update if the value is non-nil and non-empty.
-func (b *DynamicUpdateBuilder) AddString(field string, value *string) {
-	if value != nil && *value != "" {
-		b.sets = append(b.sets, field+" = ?")
-		b.args = append(b.args, *value)
-	}
-}
-
-// AddNullableString adds a string field update if the pointer is non-nil.
-func (b *DynamicUpdateBuilder) AddNullableString(field string, value *string) {
-	if value != nil {
-		b.sets = append(b.sets, field+" = ?")
-		b.args = append(b.args, *value)
-	}
-}
-
-// AddNullableInt adds an integer field update if the pointer is non-nil.
-func (b *DynamicUpdateBuilder) AddNullableInt(field string, value *int) {
-	if value != nil {
-		b.sets = append(b.sets, field+" = ?")
-		b.args = append(b.args, *value)
-	}
-}
-
-// AddField adds a raw field update with a value.
-func (b *DynamicUpdateBuilder) AddField(field string, value interface{}) {
-	b.sets = append(b.sets, field+" = ?")
-	b.args = append(b.args, value)
-}
-
-// IsEmpty returns true if no fields have been added.
-func (b *DynamicUpdateBuilder) IsEmpty() bool {
-	return len(b.sets) == 0
-}
-
-// Build constructs the UPDATE query string and returns args.
-// The caller must append the WHERE arg(s) to args.
-func (b *DynamicUpdateBuilder) Build(table string) (query string, args []interface{}) {
-	if len(b.sets) == 0 {
-		return "", nil
-	}
-	query = fmt.Sprintf("UPDATE %s SET %s WHERE ", table, strings.Join(b.sets, ", "))
-	return query, b.args
-}
-
-// BuildWithTimestamp is like Build but also adds an updated_at = CURRENT_TIMESTAMP clause.
-func (b *DynamicUpdateBuilder) BuildWithTimestamp(table string) (query string, args []interface{}) {
-	b.AddField("updated_at", "CURRENT_TIMESTAMP")
-	return b.Build(table)
 }
 
 // QueryExec executes the built update and returns rows affected.
